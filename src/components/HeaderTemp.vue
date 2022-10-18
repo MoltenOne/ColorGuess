2252b09d13324505ee207d57811f7a273f933042
MoltenOne <alexandernovoselov@mail.ru>
2022-10-18 20:04:12 +0300

    0.0.1

---
+++ src/components/HeaderTemp.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+/*defineProps({
+  news: Number
+})*/
+
+const newsCount = ref(0)
+</script>
+
+<template>
+<div>
+    <div>Количество новостей: {{newsCount}}</div>
+    <span>Главная</span> 
+    <span>Цветы</span> 
+    <span>Деревья</span> 
+    <span>Кустарники</span> 
+</div>
+</template>
+
+<style scoped>
+
+span {
+  width:100px;
+  height:40px;
+  display: inline-block;
+}
+
+</style>
