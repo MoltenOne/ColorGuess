e235acda4fb1f1898ad7188e9a1316781d979388
MoltenOne <alexandernovoselov@mail.ru>
2022-10-19 21:02:27 +0300

    0.1.0
    Main menu realized

--- src/components/HeaderTemp.vue
+++ src/components/HeaderTemp.vue
@@ -1,29 +1,84 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
-/*defineProps({
-  news: Number
-})*/
 
-const newsCount = ref(0)
+const difVal = ref(0)
+
+
+
 </script>
 
 <template>
-<div>
-    <div>Количество новостей: {{newsCount}}</div>
-    <span>Главная</span> 
-    <span>Цветы</span> 
-    <span>Деревья</span> 
-    <span>Кустарники</span> 
+<div class = "mainDiv">
+<span class = "difSpan">Choose difficulty</span>
+  <div class = "topMainDiv">
+      
+      <div class="starDiv" v-if="difVal > 0" @click = "difVal = 1">
+          <img src="../assets/star-icon.svg"> 
+          <div>EASY</div>
+      </div>
+      <div class="starDiv" v-else @click = "difVal = 1">
+          <img  src="../assets/star-outline.svg">
+          <div>EASY</div>
+      </div>
+      <div class="starDiv" v-if="difVal > 1" @click = "difVal = 2">
+          <img src="../assets/star-icon.svg">
+          <div>MEDIUM</div>
+      </div>
+      <div class="starDiv" v-else @click = "difVal = 2">
+          <img src="../assets/star-outline.svg">
+          <div>MEDIUM</div>
+          </div>  
+      <div class="starDiv" v-if="difVal > 2" @click = "difVal = 3">
+          <img src="../assets/star-icon.svg">
+          <div>HARD</div>
+      </div>
+      <div class="starDiv" v-else @click = "difVal = 3">
+      <img src="../assets/star-outline.svg">
+          <div>HARD</div>
+      </div> 
+  </div>
+
+  <div class = "botMainDiv">
+    <button> ИГРАТЬ</button>
+  </div>
+
 </div>
 </template>
 
 <style scoped>
 
+.difSpan{
+  vertical-align: top;
+  width: 300px;
+  font-size: 30px;
+}
+.starDiv {
+  height:140px;
+  display: inline-block;
+}
+
+div {
+border: 2px solid grey
+}
+
+img {
+  height: 100px;
+  width: 100px;
+  vertical-aligh:top;
+}
+
+img:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+
 span {
   width:100px;
-  height:40px;
+  height:100px;
   display: inline-block;
+  margin:20px;
+  border: 2px solid aqua
 }
 
 </style>
